f64440b27953da6b83f79b135a495f1c01893204
JasmineIslamNY <jasmine@tek-genie.com>
2014-12-11 20:29:42 -0500

    201412122_2029

--- Bicycle/Jasmines_Bike.py
+++ Bicycle/Jasmines_Bike.py
@@ -1,6 +1,6 @@
 from bicycle import * 
 
-#initializing bikes
+#instantiating bikes
 buster = Bicycle("Buster", 75, 100)
 manx = Bicycle("Manx", 70, 150)
 shadow = Bicycle("Shadow", 60, 300)
@@ -8,60 +8,29 @@ storm = Bicycle("Storm", 50, 600)
 wallaby = Bicycle("Wallaby", 25, 800)
 zeus = Bicycle("Zeus", 15, 2000)
 
+#instantiating shop
 jasmines_stock = [[buster, 10], [manx, 20], [shadow, 5], [storm, 5], [wallaby, 10], [zeus, 1]]
-
-#initializing shop
 shop = BikeShop("Jasmines Bike Shop", jasmines_stock)
 shop.margin = .20
 
-#initializing customers
+#instantiating customers
 john = BikeCustomer("John Smith", 200, "none")
 sally = BikeCustomer("Sally Lions", 500, "none")
 link = BikeCustomer("Link Bait", 1000, "none") 
+ 
+#print the bikes the customers can afford from the bike shop
+john.bike_options(shop)
+sally.bike_options(shop)
+link.bike_options(shop)
 
-""" Testing 
-# this works to print the attributes of the bicycle objects
-for item in shop.inventory:
-	print item[0].name, item[0].weight, item[0].cost
-
-# this gives the bike name and selling price
-for item in shop.inventory:
-	print item[0].name, shop.bike_price(item[0].cost)
-	
- Testing """
-#print the bikes the customers can afford
-customers = [john, sally, link]
-for customer in customers:
-	print "These are the bikes that {} can afford:".format(customer.name)
-	for item in shop.inventory:
-		if (shop.bike_price(item[0].cost)) < customer.budget:
-			print "Bike Model: {} Price: ${}".format(item[0].name, shop.bike_price(item[0].cost))
-	print " "
 # print bike shop inventory before customers buy bikes
-print "{} has: ".format(shop.name)
-for item in shop.inventory:
-	print "{} {}'s ".format(item[1], item[0].name),
-print " "
-print " "
-
+shop.print_inventory()
 	
-# each customer buys a bike they can afford
-def cust_buys(customer, bike, price):
-	print "{} has ${} in the bank".format(customer.name, customer.budget)
-	customer.buy_bike(bike, price)
-	shop.sell_bike(bike, price)
-	print "{} just bought a {} for ${}!".format(customer.name, customer.bike_owned, price)
-	print "{} now has ${} in the bank.".format(customer.name, customer.budget)
-	print " "
-	
-cust_buys(john, "Buster", 120)
-cust_buys(sally, "Shadow", 360)
-cust_buys(link, "Wallaby", 960)
+# each customer buys a bike they can afford	from shop
+john.buy_bike("Buster", 120, shop)
+sally.buy_bike("Shadow", 360, shop)
+link.buy_bike("Wallaby", 960, shop)
 
 # print bike shop inventory after customers buy bikes and how much profit it made
-print "{} now has: ".format(shop.name)
-for item in shop.inventory:
-	print "{} {}'s ".format(item[1], item[0].name),
-print " "
-print "{} made ${} in profit!".format(shop.name, shop.profit)
-print " "
+shop.print_inventory()
+shop.print_profit()
--- Bicycle/bicycle.py
+++ Bicycle/bicycle.py
@@ -1,11 +1,3 @@
-class Bicycle(object):
-	""" Bicycle need arguments name, weight, and cost """
-	def __init__(self, name, weight, cost):
-		self.name = name
-		self.weight = weight
-		self.cost = cost
-	
-	
 class BikeShop(object):
 	"""BikeShop has a list called 'inventory', arguments name and inventory, and methods
 	bike_price (self, cost) and sell_bike (bike_name, price) """
@@ -29,18 +21,47 @@ class BikeShop(object):
 		for item in self.inventory:
 			if item[0].name == bike_name:
 				item[1] -= 1
-
+	
+	def print_inventory(self):
+		print "{} has: ".format(self.name)
+		for item in self.inventory:
+			print "{} {}'s ".format(item[1], item[0].name),
+		print " "
+		print " "
+	
+	def print_profit(self):
+		print "{} made ${} in profit!".format(self.name, self.profit)
+		print " "
+	
+class Bicycle(BikeShop):
+	""" Bicycle need arguments name, weight, and cost """
+	def __init__(self, name, weight, cost):
+		self.name = name
+		self.weight = weight
+		self.cost = cost
+	
 		
-class BikeCustomer(object):
+class BikeCustomer(BikeShop):
 	"""BikeCustomer has name, budget, bike_owned and a method called buy_bike with bike_name and price"""
 	def __init__(self, name, budget, bike_owned):
 		self.name = name
 		self.budget = budget
 		self.bike_owned = bike_owned
 	
-	def buy_bike(self, bike_name, price):
+	def buy_bike(self, bike_name, price, shopname):
+		print "{} has ${} in the bank".format(self.name, self.budget)
 		self.bike_owned = bike_name
 		self.budget = self.budget - price
-		# BikeShop.sell_bike(bike_name, price)
+		shopname.sell_bike(bike_name, price)
+		print "{} just bought a {} for ${} from {}!".format(self.name, self.bike_owned, price, shopname.name)
+		print "{} now has ${} in the bank.".format(self.name, self.budget)
+		print " "
 		
-	
+	def bike_options(self, shopname):
+		print "These are the bikes that {} can afford from {}:".format(self.name, shopname.name)
+		for item in shopname.inventory:
+			if (shopname.bike_price(item[0].cost)) < self.budget:
+				print "Bike Model: {} Price: ${}".format(item[0].name, shopname.bike_price(item[0].cost))
+		print " "
+		
+		
