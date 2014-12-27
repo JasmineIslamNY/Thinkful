50573c62d612a2831f9da53362b1efc7dc6276fb
JasmineIslamNY <jasmine@tek-genie.com>
2014-12-27 10:19:00 -0500

    Revert "20141227_1016"
    This reverts commit 7ba18457d87af970463d3b60f5f2b8be02f9b885.

--- github_languages/github_languages.py
+++ github_languages/github_languages.py
@@ -1,20 +1,19 @@
 import sys
 import operator
 from collections import defaultdict
-from secret import USERNAME, PASSWORD	
 
 import requests
 
 def get_repositories(user):
     """ Retreive a list of a user's repositories """
     url = "https://api.github.com/users/{user}/repos".format(user=user)
-    response = requests.get(url, auth=(USERNAME, PASSWORD))
+    response = requests.get(url)
     return response.json()
 
 def main():
-	""" Main function """
-	repositories = get_repositories(sys.argv[1])
-	print repositories
+    """ Main function """
+    repositories = get_repositories(sys.argv[1])
+    print repositories
 
 if __name__ == "__main__":
     main()
