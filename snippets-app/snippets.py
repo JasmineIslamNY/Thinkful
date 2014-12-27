5af4c6c2ab2f77918cdbc51a8287d8c84d8a5f51
JasmineIslamNY <jasmine@tek-genie.com>
2014-12-26 20:58:26 -0500

    20141226_2058

--- snippets-app/snippets.py
+++ snippets-app/snippets.py
@@ -21,13 +21,36 @@ def get(name, filename):
 	""" Get a snippet from a file when the snippet name is listed """
 	logging.info("Retrieving {!r} from {!r}".format(name, filename))
 	logging.debug("Reading the file")
+	count = 0
 	with open(filename, "r") as file:
 		reader = csv.reader(file)
 		for row in reader:
 			if row[0] == name:
-				print "The snippet for {} is {}".format(row[0], row[1])
-	return name, filename
-	
+				print "The snippet for {!r} is {!r}".format(row[0], row[1])
+				count += 1
+	if count == 1:
+		instance = "one instance"
+	else:
+		instance = str(count) + " instances"
+	return name, filename, instance
+
+def find(snippet, filename):
+	""" Get a snippet and name from a file when the snippet is listed """
+	logging.info("Looking for {!r} from {!r}".format(snippet, filename))
+	logging.debug("Reading the file")
+	count = 0
+	with open(filename, "r") as file:
+		reader = csv.reader(file)
+		for row in reader:
+			if row[1].count(snippet) > 0:
+				print "Found {!r} in {!r}: the full snippet is {!r}".format(snippet, row[0], row[1])
+				count += 1
+	if count == 1:
+		instance = "one snippet"
+	else:
+		instance = str(count) + " snippets"
+	return filename, instance
+		
 def make_parser():
 	""" Construct the command line parser """
 	logging.info("Constructing parser")
@@ -49,6 +72,13 @@ def make_parser():
 	get_parser.add_argument("name", help = "The name of the snippet")
 	get_parser.add_argument("filename", default= "snippets.csv", nargs = "?", help = "The snippet filename")
 	
+	
+	# Subparser for the find command
+	logging.debug("Constructing find subparser")
+	find_parser = subparsers.add_parser("find", help = "Retrieve a snippet")
+	find_parser.add_argument("snippet", help = "A segment of the snippet")
+	find_parser.add_argument("filename", default= "snippets.csv", nargs = "?", help = "The snippet filename")
+	
 	return parser
 	
 def main():
@@ -65,8 +95,12 @@ def main():
 		print "Stored {!r} as {!r}".format(snippet, name)
 		
 	if command == "get":
-		name, filename = get(**arguments)
-		print "Retrieved {!r} from {!r}".format(name, filename)
+		name, filename, instance = get(**arguments)
+		print "Retrieved {} of {!r} from {!r}".format(instance, name, filename)
+	
+	if command == "find":
+		filename, instance = find(**arguments)
+		print "Retrieved {} from {!r}".format(instance, filename)
 	
 if __name__ == "__main__":
 	main()
