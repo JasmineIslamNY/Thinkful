2b23f1f8e8731efcf65fbf6d85a6becdd2ef885b
JasmineIslamNY <jasmine@tek-genie.com>
2015-01-03 14:02:45 -0500

    20150103_1402

--- tweetful/urls.py
+++ tweetful/urls.py
@@ -1,4 +1,4 @@
-API_URL = "https://api.twiiter.com"
+API_URL = "https://api.twitter.com"
 REQUEST_TOKEN_URL = API_URL + "/oauth/request_token"
 AUTHORIZE_URL = API_URL + "/oauth/authorize?oauth_token={request_token}"
 ACCESS_TOKEN_URL = API_URL + "/oauth/access_token"
