609c36077e52dbdd1b32b36b005d78e3abfeef15
JasmineIslamNY <jasmine@tek-genie.com>
2015-01-06 20:04:27 -0500

    20150106_2004

--- tweetful/authorization.py
+++ tweetful/authorization.py
@@ -30,8 +30,8 @@ def get_access_token(request_token, request_secret, verifier):
 	oauth = OAuth1(CLIENT_KEY, client_secret=CLIENT_SECRET, resource_owner_key=request_token, resource_owner_secret=request_secret, verifier=verifier)
 	response = requests.post(ACCESS_TOKEN_URL, auth=oauth)
 	credentials = urlparse.parse_qs(response.content)
-	access_token = credentials.get("oauth_token")[0]
-	access_secret = credentials.get("oauth_token_secret")[0]
+	access_token = credentials.get('oauth_token')[0]
+	access_secret = credentials.get('oauth_token_secret')[0]
 	return access_token, access_secret
 
 def authorize():
