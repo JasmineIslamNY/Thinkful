f72363c7bf02035c68e88c5ed3f5e5b4f2067ca4
jasmineislamNY <jasmine@tek-genie.com>
2015-01-21 00:09:14 -0500

    20150121_0009

---
+++ psycopg_lesson/pets(also doesn't work).py
@@ -0,0 +1,38 @@
+import csv
+ 
+#----------------------------------------------------------------------
+def csv_to_dictionary():
+    """ Convert csv file to dictionary"""
+    with open("pets.csv") as f:
+        dictionary = csv.DictReader(f, delimiter=',')
+        return dictionary
+ 
+def make_into_title(word):
+    word = word.title()
+    return word
+ 
+def remove_whitespace(word):
+    word = word.strip()
+    return word
+
+def make_upper(word):
+    word = word.upper()
+    return word
+
+if __name__ == "__main__":
+    dictionary = csv_to_dictionary()
+    for line in dictionary:
+        line["Name"] = remove_whitespace(line["Name"])
+        line["Name"] = make_into_title(line["Name"])
+        line["breed name"] = remove_whitespace(line["breed name"])
+        line["breed name"] = make_into_title(line["breed name"])
+        line["species name"] = remove_whitespace(line["species name"])
+        line["species name"] = make_into_title(line["species name"])
+        line["shelter name"] = make_upper(line["shelter name"])
+    for line in dictionary:
+        print(line["Name"]),
+        print(line["age"]),
+        print(line["breed name"]),
+        print(line["species name"]),
+        print(line["shelter name"]),
+        print(line["adopted"])
