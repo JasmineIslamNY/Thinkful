f8e46008049caab39b2943b2c792163fc4e2bea5
Jasmine <jasmine@tek-genie.com>
2015-01-22 03:39:07 +0000

    20150121_2239

--- psycopg_lesson/load_db.py
+++ psycopg_lesson/load_db.py
@@ -4,6 +4,10 @@ def update_db():
     pass     
      
 def load_db(payload):
+    dictionary = []
+    dictonary = payload
+    print dictionary
+    
     try:
         conn=psycopg2.connect("dbname='pets' user='action' password='731Lexington'")
         print "I connected"
@@ -11,11 +15,20 @@ def load_db(payload):
         print "I am unable to connect to the database."
     cur = conn.cursor()
     try:
-        cur.execute("""SELECT * from species""")
+        cur.executemany("""INSERT INTO pets (name, age, breed_id, shelter_id, adopted) VALUES (%(Name)s, %(age)s, (select breed_id from breed where breed_name = %(breed_name)s), (select shelter_id from shelter where shelter_name = %(shelter_name)s), %(adopted)s)""", payload)
     except:
         print "I can't SELECT from bar"
+    # else cur.commit()
+    
+    cur = conn.cursor()
+    try:
+        cur.execute("""SELECT * from pets""")
+    except:
+        print "I can't SELECT from pets"
     
     rows = cur.fetchall()
     for row in rows:
         print "   ", row
+        
+    return payload
     
--- psycopg_lesson/pets.py
+++ psycopg_lesson/pets.py
@@ -1,5 +1,6 @@
 import csv
-import load_db
+#from load_db import load_db
+import psycopg2
  
 def csv_to_dictionary(csv_file):
     """ Convert csv file to dictionary"""
@@ -22,25 +23,55 @@ def clean_up_dictlist(listtoclean):
     for line in listtoclean:
         line["Name"] = remove_whitespace(line["Name"])
         line["Name"] = make_into_title(line["Name"])
-        line["breed name"] = remove_whitespace(line["breed name"])
-        line["breed name"] = make_into_title(line["breed name"])
-        line["species name"] = remove_whitespace(line["species name"])
-        line["species name"] = make_into_title(line["species name"])
-        line["shelter name"] = make_upper(line["shelter name"])
+        line["breed_name"] = remove_whitespace(line["breed_name"])
+        line["breed_name"] = make_into_title(line["breed_name"])
+        line["species_name"] = remove_whitespace(line["species_name"])
+        line["species_name"] = make_into_title(line["species_name"])
+        line["shelter_name"] = make_upper(line["shelter_name"])
     return listtoclean
      
 def print_dictionary(dictlist):
     for line in dictlist:
         print(line["Name"]),
         print(line["age"]),
-        print(line["breed name"]),
-        print(line["species name"]),
-        print(line["shelter name"]),
+        print(line["breed_name"]),
+        print(line["species_name"]),
+        print(line["shelter_name"]),
         print(line["adopted"])      
      
 def update_db():
     pass     
-         
+ 
+def load_db(payload):
+    print payload
+    
+    try:
+        conn=psycopg2.connect("dbname='pets' user='action' password='731Lexington'")
+        print "I connected"
+    except:
+        print "I am unable to connect to the database."
+    cur = conn.cursor()
+    try:
+        cur.execute("""INSERT INTO pet (name, age, breed_id, shelter_id) VALUES ('Titch', 1, 1, 1)""")      
+        # cur.executemany("""INSERT INTO pet (name, age, breed_id, shelter_id, adopted) VALUES (%(Name)s, %(age)s, (select breed_id from breed where breed_name = %(breed_name)s), (select shelter_id from shelter where shelter_name = %(shelter_name)s), bool(%(adopted)s)""", payload)
+    except:
+        print "I can't Insert Into pet"
+    # else cur.commit()
+    
+    cur = conn.cursor()
+    try:
+        cur.execute("""SELECT * from pet""")
+    except:
+        print "I can't SELECT from pet"
+    
+    rows = cur.fetchall()
+    for row in rows:
+        print "   ", row
+        
+    return payload
+    
+
+
 if __name__ == "__main__":
     dictlist = []
     with open("pets.csv") as f:
@@ -49,5 +80,5 @@ if __name__ == "__main__":
             dictlist.append(line)
     dictlist = clean_up_dictlist(dictlist)
     # print_dictionary(dictlist)
-    load_db(dictlist)
+    dictlist = load_db(dictlist)
     update_db()
