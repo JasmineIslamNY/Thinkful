bf5841fe858387a803e4d85883283079abfb8354
Jasmine <jasmine@tek-genie.com>
2015-01-23 01:33:44 +0000

    20150122_2034

--- psycopg_lesson/pets.py
+++ psycopg_lesson/pets.py
@@ -107,10 +107,10 @@ def load_db(payload):
             #cur.execute("""INSERT INTO pet (name, age, breed_id, shelter_id) VALUES ('Titch', 1, 1, 1)""")       
             #cur.execute("INSERT INTO pet (name, age) VALUES (%(Name)s, %(age)s)", line)
             cur.execute("INSERT INTO pet (name, age, shelter_id, breed_id, adopted) VALUES (%(Name)s, %(age)s, (select id from shelter where name = %(shelter_name)s), (select id from breed where species_id in (select id from species where name = %(species_name)s) and name = %(breed_name)s), bool(%(adopted)s))", line)
-            print "Ran statement INSERT INTO pet (name, age, shelter_id, breed_id, adopted) VALUES ({Name}, {age}, (select id from shelter where name = {shelter_name}), (select id from breed where species_id in (select id from species where name = {species_name}) and name = {breed_name}), bool({adopted}))".format(line)
+            #print "Ran statement INSERT INTO pet (name, age, shelter_id, breed_id, adopted) VALUES ({Name}, {age}, (select id from shelter where name = {shelter_name}), (select id from breed where species_id in (select id from species where name = {species_name}) and name = {breed_name}), bool({adopted}))".format(line)
         except:
             print "I can't insert into pet, rolling back all inserts"
-            conn.rollback()
+            #conn.rollback()
     conn.commit()
           
     try:
