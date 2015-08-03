dc9edc40419aa92ea5ba96a4707a7dd679f28e9a
JasmineIslamNY <jasmine@tek-genie.com>
2015-08-03 00:58:44 -0400

    20150803_0058
    Homework final

--- Algorithms/HW1/HW1.py
+++ Algorithms/HW1/HW1.py
@@ -1,47 +1,44 @@
-from random import randint
+from array import *
+import argparse
 
-def create_randomint_array(array_size, number_size):
-	"""create array with specified size and fill with a random integer from 0 to number_size
-	"""
-	list = []
-	for i in range(array_size):
-		list.append(randint(0,number_size))
-	return list
+def make_parser():
+    """ Construct the command line parser """
+    description = "Create a specified sized list and fill with random numbers from 0 to specified limit"
+    parser = argparse.ArgumentParser(description = description)
+    parser.add_argument("ListSize", help="The size of the list you want to create, default is 5")
+    parser.add_argument("UpperLimit", help="Enter the upper limit of the random numbers, default is 100")
+    return parser
 
-def print_array(array):
-	for i in range(len(array)):
-		print array[i]
-
-def array_sort(array):
-	currIndex = 1
-	temp = 0
-	
-	if (len(array) == 1) | (len(array) == 0):
-		return array
-
-	while currIndex < len(array):
-		follower = currIndex
-		while follower > 0:
-			if array[follower - 1] > array[follower]:
-				temp = array[follower - 1]
-				array[follower - 1] = array[follower]
-				array[follower] = temp
-			follower -= 1
-		currIndex += 1		
-	return array
+def main():
+	parser = make_parser()
+	args = parser.parse_args()
 
 
+	try:
+		list_size = int(args.ListSize)
+		upper_limit = int(args.UpperLimit)
 
-if __name__ == "__main__":
-	#create array (list) of specified size
-	array = create_randomint_array(5, 100)
+		#create array (list) of specified size
+		myArray = Array.create_randomint_array(list_size, upper_limit)
 
+	except :
+		print("One of the entries was not an integer, using defaults")
+		myArray = Array.create_randomint_array(5, 100)
+    
 	print("Array before the sort")
-	print_array(array)
+	Array.print_array(myArray)
 
 	#sort the array
-	array_sort(array)
+	Array.array_sort(myArray)
 
 	print("Array after the sort")
-	print_array(array)
+	Array.print_array(myArray)
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
 
---
+++ Algorithms/HW1/array.py
@@ -0,0 +1,40 @@
+from random import randint
+
+class Array(object):
+
+	def __init__(self, name):
+		self.name = name
+
+	@staticmethod
+	def create_randomint_array(array_size, number_size):
+		"""create array with specified size and fill with a random integer from 0 to number_size
+		"""
+		list = []
+		for i in range(array_size):
+			list.append(randint(0,number_size))
+		return list
+
+	@staticmethod
+	def print_array(array):
+		for i in range(len(array)):
+			print array[i]
+
+	@staticmethod
+	def array_sort(array):
+		currIndex = 1
+		temp = 0
+	
+		if (len(array) == 1) | (len(array) == 0):
+			return array
+
+		while currIndex < len(array):
+			follower = currIndex
+			while follower > 0:
+				if array[follower - 1] > array[follower]:
+					temp = array[follower - 1]
+					array[follower - 1] = array[follower]
+					array[follower] = temp
+				follower -= 1
+			currIndex += 1		
+		return array
+
