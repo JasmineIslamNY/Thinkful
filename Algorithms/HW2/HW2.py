bd51abb0c500f03acbda4e42d9f13c56046afb54
JasmineIslamNY <jasmine@tek-genie.com>
2015-08-08 09:44:18 -0400

    20150808_944
    HW2 Try

---
+++ Algorithms/HW2/HW2.py
@@ -0,0 +1,44 @@
+from array import *
+import argparse
+
+def make_parser():
+    """ Construct the command line parser """
+    description = "Create a specified sized list and fill with random numbers from 0 to specified limit"
+    parser = argparse.ArgumentParser(description = description)
+    parser.add_argument("-ListSize", nargs='?', default = 5, help="The size of the list you want to create, default is 5")
+    parser.add_argument("-UpperLimit", nargs='?', default = 100, help="Enter the upper limit of the random numbers, default is 100")
+    return parser
+
+def main():
+	parser = make_parser()
+	args = parser.parse_args()
+
+
+	try:
+		list_size = int(args.ListSize)
+		upper_limit = int(args.UpperLimit)
+
+		#create array (list) of specified size
+		myArray = Array.create_randomint_array(list_size, upper_limit)
+
+	except :
+		print("One of the entries was not an integer, using defaults")
+		myArray = Array.create_randomint_array(5, 100)
+    
+	print("Array before the sort")
+	Array.print_array(myArray)
+
+	#sort the array
+	Array.array_sort_merge(myArray, 0, list_size)
+
+	print("Array after the sort")
+	Array.print_array(myArray)
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
---
+++ Algorithms/HW2/array.py
@@ -0,0 +1,92 @@
+from random import randint
+
+class Array(object):
+
+	def __init__(self, name):
+		self.name = name
+
+	@staticmethod
+	def create_randomint_array(array_size, number_size):
+		"""create array with specified size and fill with a random integer from 0 to number_size
+		"""
+		list = []
+		for i in range(array_size):
+			list.append(randint(0,number_size))
+		return list
+
+	@staticmethod
+	def print_array(array):
+		for i in range(len(array)):
+			print array[i]
+
+	@staticmethod
+	def array_sort_bubble(array):
+		currIndex = 1
+		temp = 0
+	
+		if (len(array) == 1) | (len(array) == 0):
+			return array
+
+		while currIndex < len(array):
+			follower = currIndex
+			while follower > 0:
+				if array[follower - 1] > array[follower]:
+					temp = array[follower - 1]
+					array[follower - 1] = array[follower]
+					array[follower] = temp
+				else:
+					break
+				follower -= 1
+			currIndex += 1		
+		return array
+
+	@staticmethod
+	def array_sort_merge(array, low, high):
+		if (len(array) == 1)| (len(array) == 0):
+			return array
+
+		mid = ((high - low)/2) + low
+		print ("Low is {}".format(low))
+		print ("Mid is {}".format(mid))
+		print ("High is {}".format(high))
+		print ("Array is:")
+		Array.print_array(array)
+		
+
+		
+		Array.array_sort_merge(array, low, mid)
+		Array.array_sort_merge(array, mid+1, high)
+		Array.array_merge(array, low, mid, high)
+
+		return array
+
+	@staticmethod
+	def array_merge(array, low, mid, high):
+
+		temp_array = []
+		mid2 = mid + 1
+		l = low
+		m2 = mid2
+		
+		for i in range(low, high+1):
+			if l == mid2:
+				temp_array.append(array[m2])
+				m2 += 1
+			elif m2 > high:
+				temp_array.append(array[l])
+				l += 1		
+			elif array[l] <= array[m2]:
+				temp_array.append(array[l])
+				l += 1
+			else:
+				temp_array.append(array[m2])
+				m2 += 1
+			i += 1	
+
+		for i in range(0, len(temp_array)):
+			array[low] = temp_array[i]
+			low +=1
+
+		return array
+
+	
