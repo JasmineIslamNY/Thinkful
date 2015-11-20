99d219dda0b7d43805c402b107cb213b0c4d8299
JasmineIslamNY <jasmine@tek-genie.com>
2015-11-20 15:44:18 -0500

    20151120_1544
    HW9 - working inputxml

--- Algorithms/HW9/inputxml.py
+++ Algorithms/HW9/inputxml.py
@@ -30,49 +30,80 @@ class InputXML(object):
 							if textData[i:(i+len(tempListKey)+3)] == "</"+ tempListKey +">":
 								parent.kind = "list"
 								parent.name = tempListKey
-								i= i + len(tempListKey) + 3
+								#print("i before {}".format(i))
+								#print("what is tempListKey: {}".format(tempListKey))
+								i= i + len(tempListKey) + 2	#there is an i increment by 1 at the bottom
+								#print("i after {}".format(i))
 								self.counter = i
 								return parent
 							elif textData[i:(i+len(tempGroupKey)+3)] == "</"+ tempGroupKey +">":
+								#print("Using 2")
 								parent.kind = "group"
 								parent.name = tempGroupKey
-								i= i + len(tempListKey) + 3
+								#print("i before {}".format(i))
+								#print("what is tempGroupKey: {}".format(tempGroupKey))
+								i= i + len(tempGroupKey) + 2	#there is an i increment by 1 at the bottom
+								#print("i after {}".format(i))
 								self.counter = i
 								return parent
 						else:
+							#print("Using 3")
 							tracker = "creatingKey"
 					elif tracker == "creatingValue":
 						if textData[i:i+2] == '</':
 							if textData[i:(i+len(tempKey)+3)] == "</"+ tempKey +">":
+								#print("Using 4")
 								tracker = ""
 								parent.addKeyValue(tempKey, tempValue)
+								#print("i before {}".format(i))
+								#print("what is tempKey: {}".format(tempKey))
 								i = i + len(tempKey) + 2      #there is an i increment by 1 at the bottom 
+								#print("i after {}".format(i))
 								tempKey = ""
 								tempValue = ""	
-						elif textData[i:i+2] != '</':
-							print(textData[i:])
-							print("******")	
+						elif textData[i:i+2] != '</' and tempGroupKey != "":
+							#print("Using 5")
+							#print(textData[i:])
+							#print("******")	
 							childObject = InputXML(textData[i:], tempKey)
 							child = childObject.processedInput
 							tempKey = ""
 							tracker = ""
 							if child.kind == "group":
+								#print("Using 6")
 								if self.previousGroupKey != "" and self.previousGroupKey != child.name:
 									self.previousGroupKey = child.name
 								elif self.previousGroupKey == "":
 									self.previousGroupKey = child.name
 								elif self.previousGroupKey == child.name:
 									tempListKey = tempGroupKey
+							#print("i before {}".format(i))
+							#print("what is childObject.counter: {}".format(childObject.counter))
 							i = i + childObject.counter
+							#print("i after {}".format(i))
 							parent.addPair(child)
+						elif textData[i:i+2] != '</':
+							#print("Using 7")
+							#print(textData[i:])
+							#print("******")	
+							tempGroupKey = tempKey
+							tempKey = ""
+							tracker = ""
+							#print("i before {}".format(i))
+							i -= 1  		#this is to reset the count so the < is looked at again	
+							#print("i after {}".format(i))					
 				if textData[i] == '>':
+					#print("Using 8")
 					if tracker == "creatingKey":
+						#print("Using 9")
 						tracker = "creatingValue"	
 
 			else:
 				if tracker == "creatingKey":
+					#print("Using 10")
 					tempKey = tempKey + textData[i]
-				elif tracker == "creatingValue":
+				elif tracker == "creatingValue":								
+					#print("Using 11")
 					tempValue = tempValue + textData[i]     #+ "*"
 			i += 1
 		self.counter = i
