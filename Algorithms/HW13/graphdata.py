7a15a80e446583b2749a105ed195ba88e9d910dc
JasmineIslamNY <jasmine@tek-genie.com>
2015-12-29 19:03:18 -0500

    20151229_1903
    HW13

--- Algorithms/HW13/graphdata.py
+++ Algorithms/HW13/graphdata.py
@@ -1,11 +1,30 @@
 class Node(object):
-	def __init__(self, name):
-		self.name = name
-		self.edges = {}
-	def addEdge(self, distance, toCity):
-		self.edges[toCity] = distance
-
+    def __init__(self, name):
+        self.name = name
+        self.edges = {}
+		
+    def addEdge(self, distance, toCity):
+        self.edges[toCity] = distance
+ 
 class GraphData(object):
-	def __init__(self):
-		self.list = []
+    def __init__(self):
+        self.list = []
 	
+    def addCity(self, city, distance = "", toCity = ""):
+        if toCity != "":
+            try:
+                toCityObjIndex = self.list.index(toCity)
+            except ValueError:
+                newToCity = Node(toCity) 
+                self.list.append(self newToCity)
+        
+    
+        try:
+           cityObjIndex = self.list.index(city)
+        except ValueError:
+            newCity = Node(city)  
+            if distance != "":
+                newCity.addEdge(distance, toCity) 
+            self.list.append(newCity)
+            
+      
